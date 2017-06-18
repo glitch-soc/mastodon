ca964789b1c5652f04890f1d33b370bb80b1db00
no-boot-device <nobootdevice@national.shitposting.agency>
2017-06-17 21:16:14 -0500

    Update formatter.rb

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -10,8 +10,6 @@ class Formatter
   include ActionView::Helpers::TextHelper
 
   def format(status)
-    return status.text.gsub("1271ed5ef305aadabc605b1609e24c52", "") if status.text.include? "1271ed5ef305aadabc605b1609e24c52" # md5("xyzzy")
-    
     if status.reblog?
       prepend_reblog = status.reblog.account.acct
       status         = status.proper
