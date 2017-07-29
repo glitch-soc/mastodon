62cade4770a667ffda227324406ade5b1339510e
kibigo! <marrus-sh@users.noreply.github.com>
2017-07-28 18:02:22 -0700

    Moved styles/images out of common

--- app/javascript/packs/frontends/mastodon.js
+++ app/javascript/packs/frontends/mastodon.js
@@ -3,6 +3,12 @@
 
 import loadPolyfills from '../../mastodon/load_polyfills';
 
+// import default stylesheet with variables
+require('font-awesome/css/font-awesome.css');
+require('mastodon-application-style');
+
+require.context('../../images/', true);
+
 loadPolyfills().then(() => {
   require('../../mastodon/main').default();
 }).catch(e => {
