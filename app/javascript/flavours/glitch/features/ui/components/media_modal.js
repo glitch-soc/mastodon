7c7f864455dc341ca0e25ededc77b17155e6447f
Thibaut Girka <thib@sitedethib.com>
2018-07-27 11:10:40 +0200

    [Glitch] Check if router is injected in media modal
    Port f3af1a96a8456f6287c827aa96d363c92191024e to glitch-soc

--- app/javascript/flavours/glitch/features/ui/components/media_modal.js
+++ app/javascript/flavours/glitch/features/ui/components/media_modal.js
@@ -71,19 +71,23 @@ export default class MediaModal extends ImmutablePureComponent {
 
   componentDidMount () {
     window.addEventListener('keydown', this.handleKeyDown, false);
-    const history = this.context.router.history;
-    history.push(history.location.pathname, previewState);
-    this.unlistenHistory = history.listen(() => {
-      this.props.onClose();
-    });
+    if (this.context.router) {
+      const history = this.context.router.history;
+      history.push(history.location.pathname, previewState);
+      this.unlistenHistory = history.listen(() => {
+        this.props.onClose();
+      });
+    }
   }
 
   componentWillUnmount () {
     window.removeEventListener('keydown', this.handleKeyDown);
-    this.unlistenHistory();
+    if (this.context.router) {
+      this.unlistenHistory();
 
-    if (this.context.router.history.location.state === previewState) {
-      this.context.router.history.goBack();
+      if (this.context.router.history.location.state === previewState) {
+        this.context.router.history.goBack();
+      }
     }
   }
 
