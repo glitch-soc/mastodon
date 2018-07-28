glitch-soc/mastodon

commit 612b00d1bb91c6b11f00fdb273dd8cd2ca883b12

Display reply indicator even when replying to contentless toot (fixes #564)

app/javascript/flavours/glitch/features/composer/index.js
@@ -314,14 +314,14 @@ class Composer extends React.Component {
         {privacy === 'direct' ? <ComposerDirectWarning /> : null}
         {privacy === 'private' && amUnlocked ? <ComposerWarning /> : null}
         {privacy !== 'public' && APPROX_HASHTAG_RE.test(text) ? <ComposerHashtagWarning /> : null}
-        {replyContent ? (
+        {replyContent !== null && (
           <ComposerReply
             account={replyAccount}
             content={replyContent}
             intl={intl}
             onCancel={onCancelReply}
           />
-        ) : null}
+        )}
         <ComposerTextarea
           advancedOptions={advancedOptions}
           autoFocus={!showSearch && !isMobile(window.innerWidth, layout)}

commit e92dff01b9b5f633faaffbd38ecf8a703fc1bb38

Show reply indicator for empty toots, not undefined toots (fixes #610)
Fix a regression introduced by 612b00d1bb91c6b11f00fdb273dd8cd2ca883b12

app/javascript/flavours/glitch/features/composer/index.js
@@ -328,7 +328,7 @@ class Composer extends React.Component {
         {privacy === 'direct' ? <ComposerDirectWarning /> : null}
         {privacy === 'private' && amUnlocked ? <ComposerWarning /> : null}
         {privacy !== 'public' && APPROX_HASHTAG_RE.test(text) ? <ComposerHashtagWarning /> : null}
-        {replyContent !== null && (
+        {replyAccount && (
           <ComposerReply
             account={replyAccount}
             content={replyContent}