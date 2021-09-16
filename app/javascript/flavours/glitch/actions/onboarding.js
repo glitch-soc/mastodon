be12f2476db6bc6d653e8c1fda8a5965628d5c45
kibigo! <marrus-sh@users.noreply.github.com>
2021-09-15 21:50:59 -0700

    Actually INTRODUCTION_VERSION needs to be positive

--- app/javascript/flavours/glitch/actions/onboarding.js
+++ app/javascript/flavours/glitch/actions/onboarding.js
@@ -1,7 +1,7 @@
 import { changeSetting, saveSettings } from './settings';
 
 // Increment this to force the onboarding modal to show again.
-export const INTRODUCTION_VERSION = 0;
+export const INTRODUCTION_VERSION = 1;
 
 export const closeOnboarding = () => dispatch => {
   dispatch(changeSetting(['introductionVersion'], INTRODUCTION_VERSION));
