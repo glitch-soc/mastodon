922972eb309f49a14ff6ae970468013d6b511ba4
Ariadne Conill <ariadne@dereferenced.org>
2022-12-25 21:11:11 +0000

    javascript: glitch: start rendering quotes
    (cherry picked from commit 005256ae8cfa1ccd5093a7ef43c5c797c0f9348f)

--- app/javascript/flavours/glitch/components/status_content.jsx
+++ app/javascript/flavours/glitch/components/status_content.jsx
@@ -373,6 +373,21 @@ class StatusContent extends PureComponent {
       <TranslateButton onClick={this.handleTranslate} translation={status.get('translation')} />
     );
 
+    let quote = '';
+
+    if (status.get('quote', null) !== null) {
+      let quoteStatus = status.get('quote');
+      let quoteStatusContent = { __html: quoteStatus.get('contentHtml') };
+
+      quote = (
+        <div className={"status__quote"}>
+          <blockquote>
+            <div dangerouslySetInnerHTML={quoteStatusContent} />
+          </blockquote>
+        </div>
+      );
+    }
+
     if (status.get('spoiler_text').length > 0) {
       let mentionsPlaceholder = '';
 
@@ -408,6 +423,7 @@ class StatusContent extends PureComponent {
           {mentionsPlaceholder}
 
           <div className={`status__content__spoiler ${!hidden ? 'status__content__spoiler--visible' : ''}`}>
+            {quote}
             <div
               ref={this.setContentsRef}
               key={`contents-${tagLinks}`}
@@ -433,6 +449,7 @@ class StatusContent extends PureComponent {
           onMouseUp={this.handleMouseUp}
           tabIndex={0}
         >
+          {quote}
           <div
             ref={this.setContentsRef}
             key={`contents-${tagLinks}-${rewriteMentions}`}
@@ -454,6 +471,7 @@ class StatusContent extends PureComponent {
           className='status__content'
           tabIndex={0}
         >
+          {quote}
           <div
             ref={this.setContentsRef}
             key={`contents-${tagLinks}`}
