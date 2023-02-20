4d2792433f8a4ce35ac642ec87fc5b57a09d9bfb
Nick Schonning <nschonni@gmail.com>
2023-02-19 20:18:54 -0500

    [Glitch] Autofix Rubocop Style/EmptyMethod
    Port 8ef09813a2b46d99fe9c7e39beba2a68052c17d1 to glitch-soc
    
    Signed-off-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- db/post_migrate/20180813160548_post_migrate_filters.rb
+++ db/post_migrate/20180813160548_post_migrate_filters.rb
@@ -5,7 +5,6 @@ class PostMigrateFilters < ActiveRecord::Migration[5.2]
     drop_table :glitch_keyword_mutes if table_exists? :glitch_keyword_mutes
   end
 
-  def down
-  end
+  def down; end
 end
 
