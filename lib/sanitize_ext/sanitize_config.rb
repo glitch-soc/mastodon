29b322941813075f7c80e9774219aa6f79ecfb86
Plastikmensch <plastikmensch@users.noreply.github.com>
2023-07-17 16:35:05 +0200

    Use Sanitize to strip invalid codelang attributes
    Instead of passing arbitrary user input to the frontend and checking if it's valid there, use Sanitize to strip invalid code languages.
    
    For that a yaml file was added containing all valid language codes, which needs to be kept in sync with highlight.js.
    This is inconvenient, but shouldn't be a problem.
    
    Signed-off-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- lib/sanitize_ext/sanitize_config.rb
+++ lib/sanitize_ext/sanitize_config.rb
@@ -21,6 +21,17 @@ class Sanitize
       gemini
     ).freeze
 
+    # Used to validate data-codelang attributes on code tags for syntax highlighting
+    VALID_LANGUAGES = YAML.load_file(File.expand_path('../../config/code-languages.yml', __dir__))['languages'].freeze
+
+    DATA_LANG_TRANSFORMER = lambda do |env|
+      return unless env[:node_name] == 'code' && env[:node]['data-codelang'].present?
+
+      node = env[:node]
+      node.remove_attribute('data-codelang') unless VALID_LANGUAGES.include?(node['data-codelang'].downcase)
+    end
+
+
     CLASS_WHITELIST_TRANSFORMER = lambda do |env|
       node = env[:node]
       class_list = node['class']&.split(/[\t\n\f\r ]/)
@@ -104,6 +115,7 @@ class Sanitize
         IMG_TAG_TRANSFORMER,
         TRANSLATE_TRANSFORMER,
         UNSUPPORTED_HREF_TRANSFORMER,
+        DATA_LANG_TRANSFORMER,
       ]
     )
 
@@ -170,6 +182,7 @@ class Sanitize
         UNSUPPORTED_HREF_TRANSFORMER,
         LINK_REL_TRANSFORMER,
         LINK_TARGET_TRANSFORMER,
+        DATA_LANG_TRANSFORMER,
       ]
     )
   end
