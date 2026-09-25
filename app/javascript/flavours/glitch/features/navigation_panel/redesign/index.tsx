4ea141e5ad7ba691b058bd848584a8b25569f0d8
diondiondion <mail@diondiondion.com>
2026-09-25 12:00:03 +0000

    [Glitch] Redesign: Add follow requests indicator to notifications page
    Port 6511e595ecf36205a1745965de4563aaea67f9b6 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/features/navigation_panel/redesign/index.tsx
+++ app/javascript/flavours/glitch/features/navigation_panel/redesign/index.tsx
@@ -2,6 +2,8 @@ import { useCallback, useEffect } from 'react';
 
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 
+import type { Map as ImmutableMap } from 'immutable';
+
 import {
   PenNibIcon,
   HouseIcon,
@@ -13,6 +15,7 @@ import {
   PlusIcon,
 } from '@phosphor-icons/react';
 
+import { fetchFollowRequests } from '@/flavours/glitch/actions/accounts';
 import { fetchLists } from '@/flavours/glitch/actions/lists';
 import { closeNavigation } from '@/flavours/glitch/actions/navigation';
 import { fetchFollowedHashtags } from '@/flavours/glitch/actions/tags_typed';
@@ -78,13 +81,39 @@ function useFollowedHashtags() {
   return { followedHashtags: tags };
 }
 
+export function useFollowRequestsCount({
+  fetch = true,
+}: { fetch?: boolean } = {}) {
+  const followRequestsCount = useAppSelector(
+    (state) =>
+      (
+        state.user_lists.getIn(['follow_requests', 'items']) as
+          | ImmutableMap<string, unknown>
+          | undefined
+      )?.size ?? 0,
+  );
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (fetch) {
+      dispatch(fetchFollowRequests());
+    }
+  }, [dispatch, fetch]);
+
+  return followRequestsCount;
+}
+
 export function useNotificationsCount() {
   const unreadNotificationsCount = useAppSelector(
     selectUnreadNotificationGroupsCount,
   );
+  const followRequestsCount = useFollowRequestsCount();
+
   const { unreadAnnouncementCount } = useHasAnnouncements();
 
-  return unreadNotificationsCount + unreadAnnouncementCount;
+  return (
+    unreadNotificationsCount + followRequestsCount + unreadAnnouncementCount
+  );
 }
 
 const isFediverseFeedsLinkActive = (
